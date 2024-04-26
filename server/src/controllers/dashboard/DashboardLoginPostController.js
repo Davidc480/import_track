385a9a99a63d8771a31bcffb1790e1a889654d5e
David <Davidcalle.480@gmail.com>
2024-04-25 23:14:05 -0600

    token created in server

--- server/src/controllers/dashboard/DashboardLoginPostController.js
+++ server/src/controllers/dashboard/DashboardLoginPostController.js
@@ -1,27 +1,23 @@
 const { userAdmin } = require("../../database");
 const { compare } = require("../../helper/handleBcrypt");
-
+const { tokenLogin } = require("../../helper/tokenSession");
 const DashboardLoginPostController = async (user, password) => {
-  // Buscar el usuario en la base de datos
   const userBdd = await userAdmin.findOne({ where: { user } });
-  console.log(userBdd);
-  // Verificar si se encontró un usuario en la base de datos
+
   if (userBdd) {
-    // Si se encontró un usuario, verificar la contraseña
     const checkPassword = await compare(password, userBdd.password);
 
-    // Si la contraseña es correcta, retornar los datos del usuario
+    const tokenSession = await tokenLogin(user);
+
     if (userBdd && checkPassword) {
       return {
         data: user,
-        // Aquí puedes agregar el token de sesión si lo deseas
+        tokenSession,
       };
     } else {
-      // Si la contraseña es incorrecta, retornar false
       return false;
     }
   } else {
-    // Si no se encontró ningún usuario, retornar false
     return false;
   }
 };
--- server/src/models/dataBanner.js
+++ server/src/models/dataBanner.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require("sequelize");
 
-module.exports = (sequelize) => {
+module.exports = sequelize => {
   sequelize.define("dataBanner", {
     idDataBanner: {
       type: DataTypes.INTEGER,
